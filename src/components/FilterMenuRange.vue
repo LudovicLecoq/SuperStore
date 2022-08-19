9480a53deaaaaff126416c6accbc2939b066085f
LudovicLecoq <ludovic.react@gmail.com>
2022-08-19 22:37:05 +0200

    correct price range css

--- src/components/FilterMenuRange.vue
+++ src/components/FilterMenuRange.vue
@@ -3,25 +3,28 @@
         <h2> {{ minPrice }} </h2>
         <h2> {{ maxPrice }} </h2>
         <div class="filter-menu-price-slider-container">
-            <div class="progress" :style="{ left: styleMin, right:styleMax }"></div>
+
+            <div class="slider">
+                <div class="progress" :style="{ left: styleMin, right:styleMax }"></div>
+            
      
             <input 
                 type="range" 
                 class="range-min range" 
-                :min="0" 
-                :max="1000" 
+                :min="minPrice" 
+                :max="maxPrice" 
                 :value="min"
                 @input="rangeMin($event)"
             >
             <input 
                 type="range" 
                 class="range-max range" 
-                :min="2" 
-                :max="1000" 
+                :min="minPrice" 
+                :max="maxPrice" 
                 :value="max"
-                
                 @input="rangeMax($event)"
             >
+            </div>
         </div>
         <div class="hop">
             <h4> {{ min }} </h4>
@@ -33,24 +36,24 @@
 
 <script>
    import { useStore } from 'vuex';
-   import { computed, ref } from 'vue';
+   import { computed, ref, watch, reactive } from 'vue';
 export default {
     setup() {
         const store = useStore();
         const data = computed(() => store.getters.loadData);
         const minPrice = computed(()=>  Math.min(...data.value.map(el => el.price)));
         const maxPrice = computed(()=>  Math.max(...data.value.map(el => el.price)));
-        const styleMin = ref(10);
+        const styleMin = ref(1);
         const styleMax = ref(1000);
-        const max = ref (null);
-        const min = ref (null);
+        const max = ref (1000);
+        const min = ref (0);
         const gap = ref (0);
 
-        setTimeout(() => {
-            max.value = maxPrice.value
-            min.value = minPrice.value
-        }, 300);
-        
+        const prices = reactive({ 
+            actualMax: maxPrice,
+            actualMin: minPrice
+        })
+
         const rangeMin = (event) => {
        
                 if(max.value - event.target.value < gap.value ){
@@ -58,19 +61,25 @@ export default {
                 } else {
                     min.value = Math.floor(event.target.value);
                 }     
-                styleMin.value = `${2+(Number(event.target.value)  * 100 / 1000)}%`;
-                console.log(styleMin.value)
+                sliderStyleMin(event.target.value);
         }
 
         const rangeMax = (event) => {
-        
                 if(event.target.value - min.value < gap.value ){
                     event.target.value = min.value + gap.value;
                 } else {
-                    max.value =Math.round(event.target.value);
+                    max.value = Math.round(event.target.value);
                 }  
-                styleMax.value = `${ 100 - (Number(event.target.value)  * 100 / 1000)}%`;
-                console.log(styleMax.value)
+                sliderStyleMax(event.target.value);
+        }
+
+        const sliderStyleMax = (value) => {
+            styleMax.value = `${ 100 - (Number(value)  * 100 / maxPrice.value)}%`;
+        }
+
+        const sliderStyleMin = (value) => {
+            styleMin.value = `${((Number(value) - minPrice.value ) * 100 / maxPrice.value )}%`;
+            console.log(styleMin.value)
         }
 
         const submitPrice = () => {
@@ -81,7 +90,13 @@ export default {
             store.dispatch('setFilterPrice', rangePrice);       
         }
 
-
+        watch(prices, ()=> {
+            console.log("watch price!!");
+            max.value = maxPrice.value;
+            min.value = minPrice.value;
+            sliderStyleMax(max.value);
+            sliderStyleMin(min.value)
+        })
       
         return { minPrice, maxPrice, rangeMax, rangeMin, min ,max, submitPrice, styleMin, styleMax }
     },
@@ -96,21 +111,27 @@ export default {
     height: 200px;
     position: relative;
 }
+
+.filter-menu-price-slider-container {
+    margin: auto;  
+}
+
 .slider {
     height: 5px;
-    border-radius: 5px;
+    border-radius: 5px;    
     background: #ddd;
+    margin: 5px auto 0;
+    width: 175px;
+    position: relative;
 }
 
 .progress{
     height: 5px;
-    left: 2%;
-    right: 0;
+    left: 5px;
+    right: 5px;
     position: absolute;
     border-radius: 5px;
-    background: #17A288;
-    /* transform: rotateY(180deg) ;    */
-    
+    background: #17A288;   
 }
 
 .range-input{
@@ -121,10 +142,15 @@ export default {
     position: absolute;
     height: 5px;
     width: 100%;
-    left: 0;
+    top: -1px;
+    left: 0px;
     -webkit-appearance: none;
     pointer-events: none;
     background: transparent;
+}
+
+.range-min {
+    left: -2px;
 }
 
 input[type="range"]::-webkit-slider-thumb {
